023631619144059ac8fee897760238792a1a8a09
hsp <hsp@163.com>
2023-09-10 17:15:56 +0800

    first commit

--- src/main/java/com/atgugui/git/GitTest.java
+++ src/main/java/com/atgugui/git/GitTest.java
@@ -2,6 +2,7 @@ package com.atgugui.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.printf("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
